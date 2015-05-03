8372e9db7d1116a718fa863879766f559c43c0c9
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2015-05-03 02:23:33 +0100

    Update the date of the Copyright

--- src/utils.ml
+++ src/utils.ml
@@ -1,5 +1,5 @@
 (*
-Copyright (c) 2013 Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
+Copyright (c) 2013-2015 Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
