1ac493dcc5a0ef93f83464e496d75084db4b88f9
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2015-12-11 12:51:18 +0100

    Fix beta-reduction

--- src/lambda/beta.ml
+++ src/lambda/beta.ml
@@ -23,47 +23,34 @@ open Monomorphic_containers.Open
 
 open LambdaTree
 
-let replace ~from ~by =
-  let eq = Ident.Name.equal from in
-  let rec aux = function
-    | Let (name, is_rec, t, xs) when eq name -> Let (name, is_rec, aux t, xs)
-    | Let (name, is_rec, t, xs) -> Let (name, is_rec, aux t, aux xs)
-    | App (f, x) -> App (aux f, aux x)
-    | Abs (name, _, _) as t when eq name -> t
-    | Abs (name, used_vars, t) -> Abs (name, used_vars, aux t)
-    | Val name when eq name -> by
-    | Val _ | Const _ | CallForeign _ as t -> t
-    | Datatype (idx, fields) -> Datatype (idx, List.map aux fields)
-    | PatternMatching (t, results, patterns) ->
-      let aux' (vars, t) = (vars, aux t) in
-        PatternMatching (aux t, List.map aux' results, patterns)
-    | Try (t, patterns) ->
-        let aux' (vars, t) = (vars, aux t) in
-        Try (aux t, List.map aux' patterns)
-    | Fail (name, args) -> Fail (name, List.map aux args)
-    | RecordGet (t, n) -> RecordGet (aux t, n)
-  in
-  aux
+let fmt = Printf.sprintf
 
-let rec reduce = function
-  | Let (name, NonRec, t, xs) ->
-      begin match reduce t with
-      | Val _ | Datatype _ | Const _ as by -> reduce (replace ~from:name ~by xs)
-      | Let _ | App _ | Abs _
-      | PatternMatching _ | Try _ | Fail _
-      | CallForeign _ | RecordGet _ as t -> Let (name, NonRec, t, reduce xs)
-      end
+let rec reduce =
+  (* TODO: Remove impure *)
+  let fresh_ident =
+    let n = ref (-1) in
+    fun () ->
+      incr n;
+      Ident.Name.local_create ~loc:Builtins.unknown_loc (fmt "beta.%d" !n)
+  in
+  function
   | App (f, x) ->
       begin match reduce f with
-      | Abs (name, _, t) -> reduce (Let (name, NonRec, x, t))
-      | Let _ | App _ | Val _ | Datatype _
+      | Let (name, is_rec, y, t) ->
+          let ident = fresh_ident () in
+          let t = App (t, Val ident) in
+          reduce (Let (ident, NonRec, x, Let (name, is_rec, y, t)))
+      | Abs (name, _, t) ->
+          reduce (Let (name, NonRec, x, t))
+      | App _ | Val _ | Datatype _
       | Const _ | PatternMatching _ | Try _
-      | Fail _ | CallForeign _ | RecordGet _ as f -> App (f, reduce x)
+      | Fail _ | CallForeign _ | RecordGet _ as f ->
+          App (f, reduce x)
       end
   | Abs (name, used_vars, t) -> Abs (name, used_vars, reduce t)
   | Val _ | Datatype _ | Const _ as t -> t
   | CallForeign (name, ty, args) -> CallForeign (name, ty, args)
-  | Let (name, Rec, t, xs) -> Let (name, Rec, reduce t, reduce xs)
+  | Let (name, is_rec, t, xs) -> Let (name, is_rec, reduce t, reduce xs)
   | PatternMatching (t, results, patterns) ->
       let aux (vars, t) = (vars, reduce t) in
       PatternMatching (reduce t, List.map aux results, patterns)
