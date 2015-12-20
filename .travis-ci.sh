bf686164d5cb8cc0edcb5f7582c476a070a5e81b
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2015-12-20 21:06:35 +0100

    Fix and improve the travis files

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,11 +1,12 @@
 sudo add-apt-repository --yes ppa:avsm/ocaml42+opam12
 sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
-echo "deb http://llvm.org/apt/precise/ llvm-toolchain-precise-$LLVM_VERSION main" | sudo tee -a /etc/apt/sources.list
+sudo add-apt-repository --yes ppa:martin-frost/dev-packages
+sudo add-apt-repository "deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-$LLVM_VERSION main"
 wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 sudo apt-get install -qq "llvm-$LLVM_VERSION" libgc-dev clang
-sudo apt-get install -qq python-cram
+sudo apt-get install -qq cram
 export OPAMYES=1
 opam init
 eval `opam config env`
