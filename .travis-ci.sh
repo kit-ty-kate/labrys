dac0cfaf7a6397d489caaa51fff1c76e182bdd2a
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2016-11-15 21:52:07 +0100

    Improve travis (CI) script

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,20 +1,29 @@
-sudo add-apt-repository --yes ppa:avsm/ocaml42+opam12
+# Install OPAM
+wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sudo sh -s /usr/local/bin
+
+# Install Ubuntu packages
 sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
 sudo add-apt-repository --yes ppa:martin-frost/dev-packages
 sudo add-apt-repository "deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-$LLVM_VERSION main"
 wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 sudo apt-get install -qq "llvm-$LLVM_VERSION" libgc-dev clang
 sudo apt-get install -qq python-cram
-export OPAMYES=1
-opam init
+
+# Init OPAM
+opam init -y --compiler=$OCAML_VERSION
 eval `opam config env`
-# BEGIN: temporary
-opam pin add -n --kind=git ocamlbuild-pkg https://github.com/jpdeplaix/ocamlbuild-pkg
-# END: temporary
+
+# Check OPAM package description
+opam lint
+
+# Install
 opam pin add -n --kind=git cervoise .
 opam install "llvm.$LLVM_VERSION" cervoise
 
+# Run tests
 make
 make tests
+
+# Uninstall
+opam remove -y cervoise
