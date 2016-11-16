01b0978dffd5c8d6e4214197ed67a1ef800996a1
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2016-11-16 22:34:39 +0100

    Improve travis (CI) script

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,18 +1,31 @@
-# Install OPAM
-wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sudo sh -s /usr/local/bin
+# Init OPAM bin directory
+mkdir -p $HOME/opam-bin
+export PATH="$HOME/opam-bin:$PATH"
+
+# Install & Init OPAM if not cached
+if type opam; then
+    cp -r $HOME/opam-cache $HOME/.opam
+else
+    # Install & Init OPAM
+    wget "https://github.com/ocaml/opam/releases/download/1.2.2/opam-1.2.2-x86_64-Linux" -O $HOME/opam-bin/opam
+    chmod u+x $HOME/opam-bin/opam
+    opam init -y --compiler=$OCAML_VERSION
+
+    # Save for cache
+    cp -r $HOME/.opam $HOME/opam-cache
+fi
+
+eval `opam config env`
 
 # Install Ubuntu packages
-sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
-sudo add-apt-repository --yes ppa:martin-frost/dev-packages
+sudo add-apt-repository --yes ppa:likemartinma/devel # cmake
+sudo add-apt-repository --yes ppa:martin-frost/dev-packages # cram
 sudo add-apt-repository "deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-$LLVM_VERSION main"
 wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo apt-get update -qq
+sudo apt-get install -qq cmake
 sudo apt-get install -qq "llvm-$LLVM_VERSION" libgc-dev clang
 sudo apt-get install -qq python-cram
-
-# Init OPAM
-opam init -y --compiler=$OCAML_VERSION
-eval `opam config env`
 
 # Check OPAM package description
 opam lint
