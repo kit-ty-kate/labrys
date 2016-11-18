948018d4b2ef0321ac1312610e5974e8d8c609b7
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2016-11-18 17:02:51 +0100

    Split the travis script in two

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,21 +1,5 @@
-# Init OPAM bin directory
-mkdir -p $HOME/opam-bin
-export PATH="$HOME/opam-bin:$PATH"
-
-# Install & Init OPAM if not cached
-if type opam; then
-    cp -r $HOME/opam-cache $HOME/.opam
-else
-    # Install & Init OPAM
-    wget "https://github.com/ocaml/opam/releases/download/1.2.2/opam-1.2.2-x86_64-Linux" -O $HOME/opam-bin/opam
-    chmod u+x $HOME/opam-bin/opam
-    opam init -y --compiler=$OCAML_VERSION
-
-    # Save for cache
-    cp -r $HOME/.opam $HOME/opam-cache
-fi
-
-eval `opam config env`
+# Init OPAM
+source .travis-opam-init.sh
 
 # Install Ubuntu packages
 sudo add-apt-repository --yes ppa:likemartinma/devel # cmake
