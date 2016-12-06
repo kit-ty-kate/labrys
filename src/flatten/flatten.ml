cf8611281d72b212b80a27f3287187f98cc3a637
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2016-12-06 13:30:27 +0100

    FlattenTree: Propagate abstractions & aliases

--- src/flatten/flatten.ml
+++ src/flatten/flatten.ml
@@ -24,6 +24,75 @@ open Monomorphic.None
 
 open FlattenTree
 
+type value =
+  | Name of name
+  | Abstr of (name * t)
+
+let rename gamma name =
+  match LIdent.Map.find name gamma with
+  | Some (Name name) -> name
+  | Some (Abstr _) | None -> name
+
+let rec propagate' gamma = function
+  | Abs (name, t) ->
+      let t = propagate gamma t in
+      ([], Abs (name, t))
+  | Rec (name, t) ->
+      let t = propagate gamma t in
+      ([], Rec (name, t))
+  | App (x, y) ->
+      let x = rename gamma x in
+      let y = rename gamma y in
+      begin match LIdent.Map.find x gamma with
+      | Some (Abstr (name, (lets, t))) -> propagate gamma ((name, Val y) :: lets, t)
+      | Some (Name _) | None -> ([], App (x, y))
+      end
+  | Val name ->
+      let name = rename gamma name in
+      ([], Val name)
+  | Datatype (idx, args) ->
+      ([], Datatype (idx, args))
+  | CallForeign (name, ret, args) ->
+      ([], CallForeign (name, ret, args))
+  | PatternMatching (name, branches, default, tree) ->
+      let name = rename gamma name in
+      let branches = List.map (propagate gamma) branches in
+      let default = propagate gamma default in
+      ([], PatternMatching (name, branches, default, tree))
+  | Fail (name, args) ->
+      ([], Fail (name, args))
+  | Try (t, (name, t')) ->
+      let t = propagate gamma t in
+      let t' = propagate gamma t' in
+      ([], Try (t, (name, t')))
+  | RecordGet (name, idx) ->
+      let name = rename gamma name in
+      ([], RecordGet (name, idx))
+  | Const c ->
+      ([], Const c)
+  | Unreachable ->
+      ([], Unreachable)
+  | Reraise name ->
+      ([], Reraise name)
+
+and propagate gamma (lets, t) =
+  let rec aux gamma = function
+    | (name, x)::xs ->
+        let (lets1, x) = propagate' gamma x in
+        let gamma = match x with
+          | Val x -> LIdent.Map.add name (Name x) gamma
+          | Abs x -> LIdent.Map.add name (Abstr x) gamma
+          | _ -> gamma
+        in
+        let (gamma, lets2) = aux gamma xs in
+        (gamma, lets1 @ [(name, x)] @ lets2)
+    | [] ->
+        (gamma, [])
+  in
+  let (gamma, lets) = aux gamma lets in
+  let (lets_t, t) = propagate' gamma t in
+  (lets @ lets_t, t)
+
 let rec of_term = function
   | LambdaTree.Abs (name, t) ->
       let t = of_term t in
@@ -65,7 +134,7 @@ let rec of_term = function
 let of_lambda_tree top =
   let aux = function
     | LambdaTree.Value (name, t, linkage) ->
-        let t = of_term t in
+        let t = propagate LIdent.Map.empty (of_term t) in
         Value (name, t, linkage)
     | LambdaTree.Exception name ->
         Exception name
