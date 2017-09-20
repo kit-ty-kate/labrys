ac94c563ff6a8a1679ee34c41fe276820211eedf
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2017-09-20 02:12:30 +0200

    Fix compilation with OCaml < 4.05

---
+++ src/monoOpen.ml
@@ -0,0 +1,7 @@
+(* Copyright (c) 2013-2017 The Cervoise developers. *)
+(* See the LICENSE file at the top-level directory. *)
+
+(* TODO: Remove this module when the minimal OCaml version reaches 4.05 *)
+(* NOTE: This module is needed because -open doesn't support submodules *)
+
+include Monomorphic.None
