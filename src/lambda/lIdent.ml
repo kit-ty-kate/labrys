863577297ee2e4ba99c8ef3020935635bf5d07a4
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2017-12-04 19:02:43 +0000

    Fix LIdent.compare / Remove all calls to Obj.magic

--- src/lambda/lIdent.ml
+++ src/lambda/lIdent.ml
@@ -1,19 +1,16 @@
 (* Copyright (c) 2013-2017 The Cervoise developers. *)
 (* See the LICENSE file at the top-level directory. *)
 
-(** NOTE: the unused id field is needed to avoid optimization by
-    the ocaml compiler *)
-type t = {name : string; id : unit ref}
+type t = < name : string >
 
-let create name = {name; id = ref ()}
-
-let raw_ptr (x : t) =
-  Nativeint.shift_left (Nativeint.of_int (Obj.magic x)) 1
+let create name = object
+  method name = name
+end
 
 let equal = (==)
-let compare x y = Nativeint.compare (raw_ptr x) (raw_ptr y)
+let compare x y = Int.compare (Oo.id x) (Oo.id y)
 
-let to_string x = x.name
+let to_string x = x#name
 
 type tmp = t
 
