b0b67286165c34311aeb97b271e59c3f961e1ec8
Kate <kit.ty.kate@disroot.org>
2018-08-09 16:09:09 +0100

    Add libgc to the depexts

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -5,11 +5,9 @@ WORKDIR /home/opam/labrys
 RUN sudo chown -R opam:opam .
 RUN opam switch $OCAML_VERSION
 
-# Install Ubuntu packages
+# Install Ubuntu repositories
 RUN echo "deb http://llvm.org/apt/unstable/ llvm-toolchain-$LLVM_VERSION main" | sudo tee -a /etc/apt/sources.list
 RUN curl -L http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
-RUN sudo apt-get update -qq
-RUN sudo apt-get install -qq cmake "llvm-$LLVM_VERSION" libgc-dev
 
 # Check OPAM package description
 RUN opam lint --warn=-48-21-32 *.opam
@@ -19,7 +17,7 @@ RUN opam repository set-url default https://opam.ocaml.org/2.0
 RUN opam pin add -y --no-action --kind=git labrys .
 RUN opam pin add -y --no-action --kind=version llvm "${LLVM_VERSION}${LLVM_VERSION_MICRO}"
 RUN opam install -y opam-depext
-RUN opam depext -y labrys
+RUN opam depext -uy labrys
 RUN opam install -yt --deps-only labrys
 RUN opam install -yvt labrys
 RUN eval \$(opam env) && make tests
