410a36fabb9cf148d54b5a4cffcb4bedfb0a208c
Kate <kit.ty.kate@disroot.org>
2019-08-04 20:26:02 +0100

    Fix travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,24 +1,20 @@
 cat << EOF > Dockerfile
-FROM ocaml/opam2:debian-unstable
+FROM ocaml/opam2:$DISTRO
 ADD . /home/opam/labrys
 WORKDIR /home/opam/labrys
 RUN sudo chown -R opam:opam .
 RUN opam switch $OCAML_VERSION
 
-# Install Ubuntu repositories
-RUN echo "deb http://llvm.org/apt/unstable/ llvm-toolchain-$LLVM_VERSION main" | sudo tee -a /etc/apt/sources.list
-RUN curl -L http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
-
 # Check OPAM package description
 RUN opam lint --warn=-48-21-32 *.opam
 
 # Install & tests
-RUN opam repository set-url default https://opam.ocaml.org/2.0
+RUN opam repository set-url default https://opam.ocaml.org/
 RUN opam pin add -y --no-action --kind=git labrys .
-RUN opam pin add -y --no-action --kind=version llvm "${LLVM_VERSION}${LLVM_VERSION_MICRO}"
+RUN opam pin add -y --no-action --kind=version llvm ${LLVM_VERSION}
 RUN opam install -y opam-depext
 RUN opam depext -uy labrys
-RUN opam install -yt --deps-only labrys
+RUN opam install -y --deps-only labrys
 RUN opam install -yvt labrys
 RUN eval \$(opam env) && make tests
 EOF
